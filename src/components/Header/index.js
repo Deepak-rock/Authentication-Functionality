43aea6923cec5af5186b5293119cf08f63036279
Deepak-rock <deepakind777@gmail.com>
2024-10-13 21:45:09 +0530

    Test 1

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,1 +1,21 @@
 // Write your JS code here
+import {Link} from 'react-router-dom'
+import './index.css'
+
+const Header = () => (
+  <div className="header-list">
+    <ul className="header-item">
+      <li>
+        <Link to="/" className="header-link">
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link to="/about" className="header-link">
+          About
+        </Link>
+      </li>
+    </ul>
+  </div>
+)
+export default Header
